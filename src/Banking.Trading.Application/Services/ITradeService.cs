a205664689f75cc28dd3d0fe30a77282f4db5695
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-04-28 19:08:41 +0200

    Implement TradeService and ITradeService for trade operations; update Docker configuration and launch settings

---
+++ src/Banking.Trading.Application/Services/ITradeService.cs
@@ -0,0 +1,10 @@
+﻿using Banking.Trading.Domain.Aggregates;
+
+namespace Banking.Trading.Application.Services;
+
+public interface ITradeService
+{
+    Task ExecuteTrade(Trade trade);
+    Task<IEnumerable<Trade>> GetAllTrades();
+    Task<Trade?> GetTradeById(Guid id);
+}
---
+++ src/Banking.Trading.Application/Services/TradeService.cs
@@ -0,0 +1,39 @@
+﻿using Banking.Trading.Domain.Aggregates;
+using Banking.Trading.Domain.Repositories;
+
+using Microsoft.Extensions.Logging;
+
+namespace Banking.Trading.Application.Services;
+
+public class TradeService : ITradeService
+{
+    private readonly ILogger<TradeService> _logger;
+    private readonly ITradeRepository _tradeRepository;
+
+    public TradeService(ITradeRepository tradeRepository,
+        ILogger<TradeService> logger)
+    {
+        _tradeRepository = tradeRepository;
+        _logger = logger;
+    }
+
+    public async Task ExecuteTrade(Trade trade)
+    {
+        _logger.LogInformation("Executing trade: {Trade}", trade);
+        await _tradeRepository.AddTrade(trade);
+    }
+
+    public async Task<IEnumerable<Trade>> GetAllTrades()
+    {
+        _logger.LogInformation("Getting all trades");
+        var trades = await _tradeRepository.GetAllTrades();
+        return trades;
+    }
+
+    public async Task<Trade?> GetTradeById(Guid id)
+    {
+        _logger.LogInformation("Getting trade by id: {TradeId}", id);
+        var trade = await _tradeRepository.GetTradeById(id);
+        return trade;
+    }
+}
