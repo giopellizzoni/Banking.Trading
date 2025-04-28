1acfc8698687f96d6198518f9c1ea05d15ac5dd9
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-04-28 19:15:10 +0200

    Add project reference for Banking.Trading.Infrastructure in application project

--- src/Banking.Trading.Application/Services/TradeService.cs
+++ src/Banking.Trading.Application/Services/TradeService.cs
@@ -1,5 +1,7 @@
 ﻿using Banking.Trading.Domain.Aggregates;
+using Banking.Trading.Domain.Events;
 using Banking.Trading.Domain.Repositories;
+using Banking.Trading.Infrastructure.Messaging;
 
 using Microsoft.Extensions.Logging;
 
@@ -9,18 +11,26 @@ public class TradeService : ITradeService
 {
     private readonly ILogger<TradeService> _logger;
     private readonly ITradeRepository _tradeRepository;
+    private readonly IMessageBus _messageBus;
 
     public TradeService(ITradeRepository tradeRepository,
-        ILogger<TradeService> logger)
+        ILogger<TradeService> logger,
+        IMessageBus messageBus)
     {
         _tradeRepository = tradeRepository;
         _logger = logger;
+        _messageBus = messageBus;
     }
 
     public async Task ExecuteTrade(Trade trade)
     {
         _logger.LogInformation("Executing trade: {Trade}", trade);
+
         await _tradeRepository.AddTrade(trade);
+        var @event = new TradeCreatedEvent();
+
+        _logger.LogInformation("Trade created: {Trade}", @event);
+        await _messageBus.PublishMessageAsync(@event);
     }
 
     public async Task<IEnumerable<Trade>> GetAllTrades()
