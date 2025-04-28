13c28802eb79983468016384bcf863c8a76e38a3
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-04-28 15:15:39 +0200

    Add DbSet for Trade and configure model creation in TradingDbContext

--- src/Banking.Trading.Infrastructure/Data/TradingDbContext.cs
+++ src/Banking.Trading.Infrastructure/Data/TradingDbContext.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Banking.Trading.Domain.Aggregates;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Banking.Trading.Infrastructure.Data;
 
 public class TradingDbContext : DbContext
 {
+    public TradingDbContext(DbContextOptions<TradingDbContext> options): base(options)
+    {
+    }
 
+    public DbSet<Trade> Trades => Set<Trade>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TradingDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
